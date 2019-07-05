9edfa3849aef7e9f9a3ea966570b53e4ca5d57df
Chris <christopher.lovejoy1@gmail.com>
2019-07-05 21:10:00 +0100

    add links to location and recent services

--- services/urls.py
+++ services/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:service_id>/', views.detail, name='detail'),
-    path('<int:service_id>/recent/', views.recent, name='recent'),
-    path('<int:service_id>/location/', views.location, name='location'),
+    path('recent/', views.recent, name='recent'),
+    path('location/', views.location, name='location'),
 ]
