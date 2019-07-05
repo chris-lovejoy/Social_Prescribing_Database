25e60e5198b37b11a2709df05a7bca9411e60968
Chris <christopher.lovejoy1@gmail.com>
2019-07-05 18:34:05 +0100

    adding frameworks

--- services/models.py
+++ services/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+
+# Dependencies for date time field
+import datetime
+from django.utils import timezone
+
+
+
+
+#  MODELS
+
+class Service(models.Model):
+	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=1000)
+	# pub_date = models.DateTimeField('date published')
+	# exp_date = models.DateTimeField('date expired')
+	def __str__(self):
+		return self.title
+# To add more
+# ?to add 'service type' and link to a drop-down menu?/other object class
+
