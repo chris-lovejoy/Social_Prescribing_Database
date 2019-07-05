b611d778c6179cd54c55a6206bb6ee4658a52cb8
Chris <christopher.lovejoy1@gmail.com>
2019-07-05 21:02:26 +0100

    add website and image to details page

--- services/models.py
+++ services/models.py
@@ -12,8 +12,10 @@ from django.utils import timezone
 #  MODELS
 
 class Services(models.Model):
-	title = models.CharField(max_length=100)
+	title = models.CharField(max_length=50)
 	description = models.CharField(max_length=1000)
+	website = models.CharField(max_length = 100, null=True)
+	image_url = models.CharField(max_length = 250, default="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg")
 	# pub_date = models.DateTimeField('date published')
 	# exp_date = models.DateTimeField('date expired')
 	def __str__(self):
